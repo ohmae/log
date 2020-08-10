327954a8105499e8b7161a53e326717e561de8f5
OHMAE Ryosuke <ryo@mm2d.net>
2020-08-10 10:35:32 +0900

    💚prepare kts

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.android.tools.build:gradle:4.0.1")
+    implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.29.0")
+}
---
+++ buildSrc/src/main/java/build/ProjectProperties.kt
@@ -0,0 +1,17 @@
+package build
+
+object ProjectProperties {
+    const val groupId: String = "net.mm2d"
+
+    private const val versionMajor: Int = 0
+    private const val versionMinor: Int = 9
+    private const val versionPatch: Int = 1
+    const val versionName: String = "$versionMajor.$versionMinor.$versionPatch"
+    const val versionCode: Int = versionMajor * 10000 + versionMinor * 100 + versionPatch
+
+    object Url {
+        const val site: String = "https://github.com/ohmae/log"
+        const val github: String = "https://github.com/ohmae/log"
+        const val scm: String = "scm:git:https://github.com/ohmae/log.git"
+    }
+}
